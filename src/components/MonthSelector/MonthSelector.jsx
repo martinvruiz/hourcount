6306c818c5bba99317edd2a68586199ef297192a
martinvruiz <martinvruiz10@gmail.com>
2024-11-14 14:50:23 -0300

    delete btn update

--- src/components/MonthSelector/MonthSelector.jsx
+++ src/components/MonthSelector/MonthSelector.jsx
@@ -215,10 +215,15 @@ const handleMonth = (e) => {
                     <h3 className="lg:text-xl py-2">Registros del mes {month}:</h3>
                     <ul>
                         {records.map(({id,date, hours}, index) => (
-                        <div className="grid border border-white px-4 py-2 grid-cols-3 items-center justify-center w-full max-w-full" key={index}>
-                            <div className="">{date}</div>
-                            <div className="">{hours}</div>
-                            <button className="border rounded-md mx-1 bg-red-500" onClick={()=>askDelete(id)}>Eliminar</button>
+                        <div className= " text-center grid border border-white px-4 py-2 grid-cols-3 items-center justify-center w-full max-w-full" key={index}>
+                            <div className="my-1">{date}</div>
+                            <div className="my-1">{hours}</div>
+                            <button className="border rounded-md p-2 m-3 flex items-center justify-center bg-red-500" onClick={()=>askDelete(id)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="text-white bi bi-trash" viewBox="0 0 16 16">
+                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                                </svg>
+                            </button>
                         </div>
                         ))}
                         <ToastContainer position="top-center" className="text-center"/>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = ({handleLogout})=>{
         <nav className="h-16 my-2 overflow-hidden text-center w-screen flex items-center justify-center text-xl py-10 border-b border-white">
             <Link className="p-4" to="/">Hour Count</Link>
             <Link className="p-4" to="/hourqt">Cant. de horas</Link>
-            <button onClick={handleLogout} className=" bg-red-500 p-2 m-2 rounded text-white">
+            <button onClick={handleLogout} className=" bg-black p-2 m-2 rounded text-white">
         Cerrar Sesión
       </button>
       <ToastContainer position="top-center"/>
